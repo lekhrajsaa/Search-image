4e858497a550be907fc71bc2ea0a5c295b85752d
lekhrajsaini0 <lekhrajsaini700@gmail.com>
2021-08-11 19:17:50 +0530

    updated code

--- src/Store/index.jsx
+++
@@ -1,6 +0,0 @@
-import {configureStore} from '@reduxjs/toolkit'
-import ImageSlice from './ImagesSlice'
-const store = configureStore({
-    reducer : ImageSlice.reducer
-}); 
-export default store;
